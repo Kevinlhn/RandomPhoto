12d2f0a96878115369478b6a64aafe95a59c3ef2
Kevinlhn <101072161+Kevinlhn@users.noreply.github.com>
2022-12-02 15:47:40 -0800

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Kevin Hernandez on 12/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
